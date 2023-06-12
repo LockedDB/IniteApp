98a4d6dada62660843af45f69e3c57076763fa61
Daniel <daniel.benet@outlook.es>
2023-06-12 17:17:25 +0200

    add colors

--- src/Utils/colors.ts
+++ src/Utils/colors.ts
@@ -1,2 +1,7 @@
 export const Black = '#000000';
 export const White = '#FFFFFF';
+
+// System-dark
+export const Night = '#101213';
+export const EerieBlack = '#1D1F20';
+export const Gunmetal = '#2D3337';
