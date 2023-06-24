6887da51c2c057e2c40fe6b5fa8f62c2341c3f02
Daniel <daniel.benet@outlook.es>
2023-06-24 17:59:05 +0200

    install navigation

--- src/init/ApplicationWithProviders.tsx
+++ src/init/ApplicationWithProviders.tsx
@@ -1,9 +1,12 @@
 import { Provider } from 'react-redux';
 import App from '../../App';
 import { store } from '../Store/store';
+import { NavigationContainer } from '@react-navigation/native';
 
 export const ApplicationWithProviders = () => (
   <Provider store={store}>
-    <App />
+    <NavigationContainer>
+      <App />
+    </NavigationContainer>
   </Provider>
 );
