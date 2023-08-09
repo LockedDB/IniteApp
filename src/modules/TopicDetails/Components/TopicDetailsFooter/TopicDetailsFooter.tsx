4690fe9029e157fdd1bf4a2117090231f4162b87
Daniel <daniel.benet@outlook.es>
2023-08-09 15:07:23 +0200

    add a send button

--- src/modules/TopicDetails/Components/TopicDetailsFooter/TopicDetailsFooter.tsx
+++ src/modules/TopicDetails/Components/TopicDetailsFooter/TopicDetailsFooter.tsx
@@ -2,17 +2,35 @@ import { TextInput, TouchableOpacity, View } from 'react-native';
 import styles from './styles';
 import { AttachFile } from '@/assets/SVG';
 import { White300 } from '@/utils/colors';
+import { CustomText } from '@/components/CustomText';
+import { useState } from 'react';
 
-export const TopicDetailsFooter = () => (
-  <View style={styles.container}>
-    <TouchableOpacity style={styles.attachContainer}>
-      <AttachFile />
-    </TouchableOpacity>
-    <TextInput
-      placeholder="Write a message..."
-      placeholderTextColor={White300}
-      style={styles.input}
-      multiline
-    />
-  </View>
-);
+export const TopicDetailsFooter = () => {
+  const [message, setMessage] = useState('');
+
+  const onPress = () => {
+    // send message
+    setMessage('');
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TouchableOpacity style={styles.attachContainer}>
+          <AttachFile />
+        </TouchableOpacity>
+        <TextInput
+          value={message}
+          onChangeText={setMessage}
+          placeholder="Write a message..."
+          placeholderTextColor={White300}
+          style={styles.input}
+          multiline
+        />
+      </View>
+      <TouchableOpacity onPress={onPress} style={styles.sendButton}>
+        <CustomText style={styles.sendText}>Send</CustomText>
+      </TouchableOpacity>
+    </View>
+  );
+};
--- src/modules/TopicDetails/Components/TopicDetailsFooter/styles.ts
+++ src/modules/TopicDetails/Components/TopicDetailsFooter/styles.ts
@@ -1,16 +1,21 @@
-import { Gunmetal, White } from '@/utils/colors';
+import { Black, Gunmetal, White } from '@/utils/colors';
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputContainer: {
     margin: 16,
+    marginRight: 8,
     marginTop: 0,
     flexDirection: 'row',
     paddingHorizontal: 16,
     paddingVertical: 12,
     gap: 8,
-    alignSelf: 'stretch',
-
+    height: 48,
+    width: '75%',
     borderRadius: 8,
     backgroundColor: Gunmetal,
   },
@@ -21,7 +26,19 @@ export default StyleSheet.create({
     fontSize: 16,
     fontFamily: 'Roboto-Regular',
     color: White,
-    width: '90%',
     textAlign: 'justify',
+  },
+  sendButton: {
+    width: 56,
+    height: 48,
+    alignSelf: 'flex-start',
+    backgroundColor: White,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sendText: {
+    alignSelf: 'center',
+    color: Black,
   },
 });
